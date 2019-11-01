9c50fe0a02dda3b67b11730af8f1e8542f5cb364
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2019-11-01 16:56:00 +0100

    add clap and command line options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#![feature(let_chains)]
+
 #![feature(test)]
 extern crate test;
 
 use rusqlite::{Connection, NO_PARAMS};
+use clap::{Arg, App};
 
 mod assembly;
 mod attachment;
@@ -39,63 +42,121 @@ struct CompoundEntry {
 }
 
 fn main() {
-    let conn = Connection::open("sqlite/pubchem.db").unwrap();
+    let matches = App::new("moas")
+                    .version("0.1")
+                    .author("Marvin Böcker <marvin.boecker@tu-dortmund.de>")
+                    .about("brute force assembler for molecular graphs")
+                    .arg(Arg::with_name("compound id")
+                        .short("c")
+                        .long("cid")
+                        .help("Dis- and reassemble the specified compound from the PubChem database.")
+                        .takes_value(true))
+                    .arg(Arg::with_name("database file name")
+                        .short("d")
+                        .long("database")
+                        .help("Specify the name of the SQLite database containing the compound data.")
+                        .takes_value(true))
+                    .arg(Arg::with_name("min")
+                        .long("min")
+                        .help("Dis- and reassemble the compounds between min and max.")
+                        .takes_value(true))
+                    .arg(Arg::with_name("max")
+                        .long("max")
+                        .help("Dis- and reassemble the compounds between min and max.")
+                        .takes_value(true))
+                    .get_matches();
 
-    // caffeine
-    // let cid = 2519;
+    let sqlite_name = matches.value_of("database file name").unwrap_or("sqlite/pubchem.db");
+    let conn = Connection::open(sqlite_name).unwrap();
 
-    // porphorin
-    let cid = 66868;
-
-    let sql = format!("SELECT cid, structure, is_contiguous, n_atoms, n_edges FROM compounds where cid = {} LIMIT 1", cid);
-    let mut stmt = conn.prepare(&sql).unwrap();
-    let iter = stmt
-        .query_map(NO_PARAMS, |row| {
-            Ok(CompoundEntry {
-                cid: row.get(0)?,
-                structure: row.get(1)?,
-                is_contiguous: row.get(2)?,
-                atoms: row.get(3)?,
-                bonds: row.get(4)?,
+    if let Some(cid) = matches.value_of("compound id") {
+        let sql = format!("SELECT cid, structure, is_contiguous, n_atoms, n_edges FROM compounds where cid = {} LIMIT 1", cid);
+        let mut stmt = conn.prepare(&sql).unwrap();
+        let iter = stmt
+            .query_map(NO_PARAMS, |row| {
+                Ok(CompoundEntry {
+                    cid: row.get(0)?,
+                    structure: row.get(1)?,
+                    is_contiguous: row.get(2)?,
+                    atoms: row.get(3)?,
+                    bonds: row.get(4)?,
+                })
             })
-        })
-        .unwrap()
-        .map(|x| x.unwrap());
+            .unwrap()
+            .map(|x| x.unwrap());
 
-    for x in iter {
-        use crate::subgraphs::Subgraphs;
+        for x in iter {
+            use crate::subgraphs::Subgraphs;
 
-        let g = graph::Graph::new(x.structure);
+            let g = graph::Graph::new(x.structure);
 
-        if crate::statistics::trace_enabled() {
-            use std::io::Write;
-            let filename = "trace/original.dot";
-            let mut f = std::fs::File::create(filename).unwrap();
-            writeln!(&mut f, "graph g {{").unwrap();
-            g.dump(&mut f, 0, true).unwrap();
-            writeln!(&mut f, "}}").unwrap();
-        }
+            if crate::statistics::trace_enabled() {
+                use std::io::Write;
+                let filename = "trace/original.dot";
+                let mut f = std::fs::File::create(filename).unwrap();
+                writeln!(&mut f, "graph g {{").unwrap();
+                g.dump(&mut f, 0, true).unwrap();
+                writeln!(&mut f, "}}").unwrap();
+            }
 
-        // determine the graphs' subgraphs.
-        let sg = subgraphs::variants::SubgraphsAndRings::new(&g);
+            // determine the graphs' subgraphs.
+            let sg = subgraphs::variants::SubgraphsAndRings::new(&g);
 
-        if crate::statistics::trace_enabled() {
-            let filename = "trace/subgraphs.dot";
-            let f = std::fs::File::create(filename).unwrap();
-            crate::prelude::dump_set(f, sg.all_subgraphs()).unwrap();
-        }
+            if crate::statistics::trace_enabled() {
+                let filename = "trace/subgraphs.dot";
+                let f = std::fs::File::create(filename).unwrap();
+                crate::prelude::dump_set(f, sg.all_subgraphs()).unwrap();
+            }
 
-        // re-assemble the graph
-        let gs = assemble(sg);
+            // re-assemble the graph
+            let gs = assemble(sg);
 
-        assert!(gs.contains(&g));
+            assert!(gs.contains(&g));
 
-        if crate::statistics::trace_enabled() {
-            let filename = "trace/result.dot";
-            let f = std::fs::File::create(filename).unwrap();
-            crate::prelude::dump_set(f, gs.iter()).unwrap();
+            if crate::statistics::trace_enabled() {
+                let filename = "trace/result.dot";
+                let f = std::fs::File::create(filename).unwrap();
+                crate::prelude::dump_set(f, gs.iter()).unwrap();
+            }
         }
     }
 
-    crate::STATISTICS.lock().unwrap().dump();
+    if let Some(min) = matches.value_of("min") {
+        if let Some(max) = matches.value_of("max") {
+            let sql = format!("SELECT cid, structure, is_contiguous, n_atoms, n_edges FROM compounds where cid >= {} AND cid < {} LIMIT 1", min, max);
+            let mut stmt = conn.prepare(&sql).unwrap();
+            let iter = stmt
+                .query_map(NO_PARAMS, |row| {
+                    Ok(CompoundEntry {
+                        cid: row.get(0)?,
+                        structure: row.get(1)?,
+                        is_contiguous: row.get(2)?,
+                        atoms: row.get(3)?,
+                        bonds: row.get(4)?,
+                    })
+                })
+                .unwrap()
+                .map(|x| x.unwrap());
+
+            for x in iter {
+                use crate::subgraphs::Subgraphs;
+
+                let g = graph::Graph::new(x.structure);
+
+                // determine the graphs' subgraphs.
+                let sg = subgraphs::variants::SubgraphsAndRings::new(&g);
+
+                // re-assemble the graph
+                let gs = assemble(sg);
+
+                assert!(gs.contains(&g));
+
+                if crate::statistics::trace_enabled() {
+                    let filename = format!("trace/results_{}.dot", x.cid);
+                    let f = std::fs::File::create(filename).unwrap();
+                    crate::prelude::dump_set(f, gs.iter()).unwrap();
+                }
+            }
+        }
+    }
 }
